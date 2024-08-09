e6dc42c86f95c67991d926bfa6f3ff7a03e41ed5
SeM2x <houssem135.302@gmail.com>
2024-08-09 19:40:58 +0100

    2. Select some Amenities to be comfortable!

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    let amenities = []
+    $('.amenities input').change(function (e) {
+        const amenity = {
+            id: e.target.getAttribute('data-id'),
+            name: e.target.getAttribute('data-name')
+        }
+        if (e.target.checked) {
+            amenities.push(amenity);
+        } else {
+            amenities = amenities.filter(obj => obj.id !== amenity.id)
+        }
+        $('.amenities h4').text(amenities.map(amenity => amenity.name).join(', '));
+    });
+
+});
